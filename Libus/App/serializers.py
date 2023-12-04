790151f0a6da4bd327a40c2f10413fb38c8b56d9
iii123iii <117964666+iii123iii@users.noreply.github.com>
2023-12-04 14:42:10 +0200

    added like ui

--- Libus/App/serializers.py
+++ Libus/App/serializers.py
@@ -4,10 +4,14 @@ from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     author_username = serializers.SerializerMethodField()
+    likes = serializers.SerializerMethodField()
 
     def get_author_username(self, obj):
         return obj.author.username
     
+    def get_likes(self, obj):
+        return obj.liked.count()
+    
     class Meta:
         model = Post
-        fields = ('id', 'title', 'content', 'author_username', 'date_posted', 'post_image', 'is_image')
+        fields = ('id', 'title', 'content', 'author_username', 'date_posted', 'post_image', 'is_image', 'likes')
